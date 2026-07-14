44c1f64f288dbedffeab05717189a7ede093f283
jasonxzhai <jasonxzhai@tencent.com>
2026-07-14 20:05:18 +0800

    update

--- modules/darwin/secrets/default.nix
+++ modules/darwin/secrets/default.nix
@@ -23,11 +23,9 @@ in
 
         secrets = {
             "ssh/github" = mkSecret "ssh.yaml" "github" userSecret;
-            "ssh/gitee" = mkSecret "ssh.yaml" "gitee" userSecret;
             "ssh/gongfeng" = mkSecret "ssh.yaml" "gongfeng" userSecret;
             "ssh/dtm" = mkSecret "ssh.yaml" "dtm" userSecret;
-            "ssh/vm" = mkSecret "ssh.yaml" "vm" userSecret;
-            "ssh/aur" = mkSecret "ssh.yaml" "aur" userSecret;
+            "ssh/vps" = mkSecret "ssh.yaml" "vps" userSecret;
 
             "network/drcom-jlu" = mkSecret "network.yaml" "drcom-jlu" userSecret;
 
--- modules/home/common/ssh.nix
+++ modules/home/common/ssh.nix
@@ -9,12 +9,6 @@
                 Include = "~/.ssh/devcloud_config";
             };
 
-            "aur.archlinux.org" = {
-                User = "aur";
-                IdentityFile = "/run/secrets/ssh/aur";
-                IdentitiesOnly = true;
-            };
-
             "git.woa.com" = {
                 User = "git";
                 IdentityFile = "/run/secrets/ssh/gongfeng";
@@ -22,16 +16,10 @@
             };
 
             "dtm" = {
+                User = "jasonxzhai";
                 HostName = "21.214.137.243";
                 Port = 36000;
-                User = "jasonxzhai";
                 IdentityFile = "/run/secrets/ssh/dtm";
-                IdentitiesOnly = true;
-            };
-
-            "gitee.com" = {
-                User = "git";
-                IdentityFile = "/run/secrets/ssh/gitee";
                 IdentitiesOnly = true;
             };
 
@@ -41,11 +29,17 @@
                 IdentitiesOnly = true;
             };
 
-            vm = {
-                HostName = "192.168.166.128";
-                User = "yuzujr";
-                IdentityFile = "/run/secrets/ssh/vm";
+            "vps" = {
+                User = "ubuntu";
+                HostName = "123.207.16.35";
+                IdentityFile = "/run/secrets/ssh/vps";
                 IdentitiesOnly = true;
+            };
+
+            "home" = {
+                HostName = "laptop-nixos";
+                User = "yuzujr";
+                ProxyJump = "ubuntu@123.207.16.35";
             };
         };
     };
