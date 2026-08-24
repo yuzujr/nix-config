e3e707a3f2315305773667e6f9db897cdd48b7b1
yuzujr <15568103056@163.com>
2026-08-24 19:15:41 +0800

    update

--- modules/home/linux/services/nixloom.nix
+++ modules/home/linux/services/nixloom.nix
@@ -1,17 +1,17 @@
 {
-  inputs,
-  ...
+    inputs,
+    ...
 }:
 {
-  imports = [ inputs.nixloom.homeManagerModules.default ];
+    imports = [ inputs.nixloom.homeManagerModules.default ];
 
-  services.nixloom = {
-    enable = true;
-    acceleration = "cuda";
-    cudaCapabilities = [ "12.0" ];
-    images.enable = true;
-    openclaw.enable = true;
-    sillytavern.enable = true;
-    autoStart = false;
-  };
+    services.nixloom = {
+        enable = true;
+        acceleration = "cuda";
+        cudaCapabilities = [ "12.0" ];
+        images.enable = true;
+        openclaw.enable = true;
+        sillytavern.enable = true;
+        autoStart = false;
+    };
 }
